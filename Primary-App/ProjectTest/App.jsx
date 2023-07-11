ba3e14f7d812d3bc9436f52a0a68a4eee3d41d86
Eduardo Miury <edmiury@gmail.com>
2023-07-11 17:23:50 -0300

    learn

--- Primary-App/ProjectTest/App.jsx
+++ Primary-App/ProjectTest/App.jsx
@@ -1,30 +1,16 @@
-import { View, Text } from 'react-native'
 import React from 'react';
+import {
+  View
+} from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import Touch from './Comps/CompTouch/touch'
-import Modal from './Comps/CompModal/modal'
-
-
-
-const Stack = createStackNavigator();
-
-
+import AnimationLottie from './Comps/Component AnimationLottie/lottie';
 
 export default function App() {
   return (
-    
-      <NavigationContainer >
-        <Stack.Navigator>
-          <Stack.Screen name=' ' component={Touch} options={{
-            headerTransparent: true,
-            headerTintColor: 'white'
-        }} />
-        <Stack.Screen  name = 'modal' component={Modal} />
-        </Stack.Navigator>
-      </NavigationContainer>
-       
+    <View>
+
+      <AnimationLottie />
+
+    </View> 
   )
 }
---
+++ Primary-App/ProjectTest/Comps/Component AnimationLottie/lottie.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+    View,
+    Text
+} from 'react-native';
+
+import Lottie from 'lottie-react-native';
+
+export default function  AnimationLottie () {
+    return (
+        <View>
+            <Text> sbyvsaydvsdvy </Text>
+        </View>
+    )
+}
