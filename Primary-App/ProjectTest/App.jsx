9fdc7dfd7a86166632704ffa6f5f9c36baa4aab3
Eduardo Miury <edmiury@gmail.com>
2024-03-13 16:03:02 -0300

    Aprendendo React Native

--- Primary-App/ProjectTest/App.jsx
+++ Primary-App/ProjectTest/App.jsx
@@ -4,13 +4,10 @@ import {
   Text
 } from 'react-native';
 
-import AnimationLottie from './Comps/Component AnimationLottie/lottie';
-
 export default function App() {
   return (
     <View>
       <Text> sadvysdvda </Text>
-      {/* <AnimationLottie/>  */}
     </View> 
   )
 }
---
+++ Primary-App/ProjectTest/Comps/Linear Gradient/linear.jsx
@@ -0,0 +1,13 @@
+import {
+    SafeAreaView,
+    Text,
+} from 'react-native';
+
+export default function Linear() {
+
+    return (
+        <SafeAreaView>
+            <Text> Testing </Text>
+        </SafeAreaView>
+    );
+}; 
